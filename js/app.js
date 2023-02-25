6084098549f74cc2a42ad85df305be2572627b9d
Mariana Ramos <mary.programms@gmail.com>
2023-02-25 19:41:32 +0000

    api connected, pokemons are displayed

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
-const fetchPokemon = async (pokemon) => {
+const pokemonNum = document.querySelector('#pokemonNum');
+const pokemonName = document.querySelector('#pokemonName');
+const pokemonImg = document.querySelector('#pokemonImg');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
 
-    // the await keywords allows the fetch to wait until all data is "charged" up
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    console.log(APIResponse)
+console.log(pokemonName)
+
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+
+    if (APIResponse.status === 200){
+        const data = await APIResponse.json();
+        return data;
+    }
 }
 
-fetchPokemon('25');
+const renderPokemon = async (pokemon) => {
+    const data = await fetchPokemon(pokemon);
+
+    pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    pokemonNum.innerHTML = data.id;
+    pokemonName.innerHTML = data.name;
+    input.value = '';
+}
+
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    renderPokemon(input.value);
+})
